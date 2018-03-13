400b5652306ac6148f7c39ed60588cf5beb7a5fa
Helder M Adversi Jr <helderdnd@gmail.com>
2018-03-13 09:53:40 -0400

    Commit inicial. Já criei a interface da tela de login.

---
+++ app/src/main/java/com/naka/atzap/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.naka.atzap.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.naka.atzap.R
+
+class MainActivity : AppCompatActivity() {
+
+    val referencia: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        referencia.child("pontos").setValue(100)
+    }
+}
